2e8e093f26a1c3bf829281617628d0828688c984
Gabriel Mizuno <gabrielmizuno@gmail.com>
2023-06-22 22:20:15 -0300

    Add hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+import faust
+
+app = faust.App(
+    'hello-world',
+    broker='kafka://localhost:9092',
+    value_serializer='raw',
+)
+
+greetings_topic = app.topic('greetings')
+
+@app.agent(greetings_topic)
+async def greet(greetings):
+    async for greeting in greetings:
+        await greetings_topic.send(value=greeting)
+
+
