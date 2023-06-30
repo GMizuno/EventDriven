5c4754c6fc0d681f057e5076b2056b3c7e2a08d9
Gabriel Mizuno <gabrielmizuno@gmail.com>
2023-06-30 20:31:47 -0300

    Add aggrgator function using window oof 5 seconds

--- db/agg.py
+++ db/agg.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import numpy as np
 import pandas as pd
 from decouple import config
@@ -16,16 +18,19 @@ def agg_data():
     engine = create_engine(uri, echo=False)
 
     data = pd.read_sql_table('coin_price', uri, index_col=None). \
-        drop(columns=['icon', 'name', 'symbol', 'rank', 'volume', 'websiteUrl', 'event_time', 'insert_date'])
+        drop(columns=['icon', 'name', 'symbol', 'rank', 'volume', 'websiteUrl', 'insert_date'])
+    data['event_time'] = pd.to_datetime(data['event_time'])
 
-    data_agg = data.groupby('id').agg(
-        min_price=('price', np.min),
-        max_price=('price', np.max),
-        avg_price=('price', np.mean),
-        count=('price', np.count_nonzero)
-    ).reset_index().\
-        rename(columns={'id': 'name'})
+    data_agg = data.groupby('id'). \
+        rolling(window=timedelta(seconds=5), on='event_time'). \
+        agg(
+            {"price": [np.max, np.min, np.mean, np.count_nonzero]}
+        ).\
+        reset_index(level=[0,1])
+    data_agg.columns = ['_'.join(col) for col in data_agg.columns.values]
+    data_agg = data_agg.rename(columns={'id_': 'name', 'event_time_': 'event_time'})
 
     data_agg.to_sql('coin_agg', con=engine, if_exists='replace', index=False)
 
 
+agg_data()
